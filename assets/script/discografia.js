64592e919c24f6ffb19eb1c6493ad7c96aff8a75
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-10-01 14:40:12 -0300

    Tests discografia.js

--- assets/script/discografia.js
+++ assets/script/discografia.js
@@ -1,9 +1,39 @@
-const busca = document.getElementsByClassName("busca");
 
-async function buscarDiscografia(){
-    const url = "https://www.vagalume.com.br/u2/discografia/index.js"
-    const data = await fetch(url);
-    console.log(data.json());
-}
 
-buscarDiscografia();
+const searchButton = document.getElementById("fetchDiscography");
+
+searchButton.addEventListener("click", function () {
+  const albumName = document.getElementById("artistInput").value;
+
+  if (!albumName) {
+    alert("Por favor, ingresa el nombre del álbum.");
+    return;
+  }
+
+  
+  const albumSearchURL = `https://api.vagalume.com.br/search.alb?apikey=660a4395f992ff67786584e238f501aa&q=${encodeURIComponent(albumName)}&limit=5`;
+
+
+  fetch(albumSearchURL)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then(data => {
+      const albums = data.response.docs;
+
+      console.log(albums);
+      const resultElement = document.getElementById("result");
+      resultElement.innerHTML = albums.map(album => `
+        <div>
+          <p>Nombre del Álbum: ${album.title}</p>
+          <p>Banda/Artista: ${album.band}</p>
+        </div>
+      `).join("");
+    })
+    .catch(error => {
+      console.error("Error:", error);
+    });
+});
