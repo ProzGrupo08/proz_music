bf9a5b0b4acb68e453705bd8d79c1a3815c147f8
deyvid lins <deyvidg.lins@gmail.com>
2023-10-01 20:04:20 -0300

    Ajustes pg biografia

---
+++ assets/script/biografia.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+        // Seu código aqui
+    var inputElement = document.querySelector("#inputBiografia");
+
+    var valorInput = inputElement.value;
+
+
+    console.log("O valor digitado no campo de entrada é: " + valorInput);
+
+});
