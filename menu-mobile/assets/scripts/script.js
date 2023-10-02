92f3204c0e3ed10914251eca6dbfb87f9b46ed66
vinicius.m0raes <vinicius.kme@gmail.com>
2023-10-02 12:33:14 -0300

    ajuste

--- menu-mobile/assets/scripts/script.js
+++ menu-mobile/assets/scripts/script.js
@@ -1,33 +1,33 @@
 mostrarMenu.addEventListener("click", () => {
-  			menu.style.display = "flex"
-  			
-  			menu.style.right = (menu.offsetWidth * -1) + "px"
-  			
-  			mostrarMenu.style.display = "none"
-  			
-  			setTimeout(() => {
-  				menu.style.opacity = "1"
-  				
-  				menu.style.right = "0"
-  			}, 10)
-  			
-  		})
+	menu.style.display = "flex"
+
+	menu.style.right = (menu.offsetWidth * -1) + "px"
+
+	mostrarMenu.style.display = "none"
+
+	setTimeout(() => {
+		menu.style.opacity = "1"
+		
+		menu.style.right = "0"
+	}, 10)
+		
+})
 
 ocultarMenu.addEventListener("click", () => {
-  	menu.style.opacity = "0"
-  	
-  	menu.style.right = (menu.offsetWidth * -1) + "px"
-  	
-  	setTimeout(() => {
-  		menu.removeAttribute("style")
-  		mostrarMenu.removeAttribute("style")
-  	}, 200) 			
+	menu.style.opacity = "0"
+
+	menu.style.right = (menu.offsetWidth * -1) + "px"
+
+	setTimeout(() => {
+		menu.removeAttribute("style")
+		mostrarMenu.removeAttribute("style")
+	}, 200) 			
   	
 })
 
 
 btnSubmit.addEventListener("click", (e)=>{
 	e.preventDefault()
-alert("A conexão com o banco de dados ainda não está implementada.")
+	alert("A conexão com o banco de dados ainda não está implementada.")
 
 })
