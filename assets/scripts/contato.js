d4c0a3257aca926e7b251fee769e3ba41a51da24
Deyvid Lins <54068775+DeyvidLins@users.noreply.github.com>
2023-11-14 20:24:48 -0300

    Update contato.js

--- assets/scripts/contato.js
+++ assets/scripts/contato.js
@@ -1,16 +1,16 @@
-btnSubmit.addEventListener("click", (e)=>{
-	var inputNome = document.getElementById("nome");
-	var inputEmail = document.getElementById("email");
-	
-	// Verificar se os campos estão vazios
-	if (inputNome.value ==="" || inputEmail.value ==="")  {
-		alert("Campo nome e e-mail obrigatório, por favor preencha.");	
+btnSubmit.addEventListener("click", (e) => {
+    e.preventDefault(); // Evita que o formulário seja enviado, já que estamos tratando o clique manualmente
 
-	}else if (inputNome.value ==="" && inputEmail.value ===""){
-		alert("Campo nome e e-mail obrigatório, por favor preencha.");
-		
-	}else {
-		alert("Obrigado pelo seu interesse em nossa plataforma! Entraremos em contato.");	
-	}
-})
+    var inputNome = document.getElementById("nome");
+    var inputEmail = document.getElementById("email");
+    var mensagemContato = document.getElementById("mensagem-contato");
 
+    // Verificar se os campos estão vazios
+    if (inputNome.value === "" || inputEmail.value === "") {
+        mensagemContato.innerHTML = "&#9888; Campo nome e e-mail obrigatórios, por favor preencha. &#9888;";
+        mensagemContato.style.color = "#fff"; // Define a cor da mensagem
+    } else {
+        mensagemContato.innerHTML = "Obrigado pelo seu interesse em nossa plataforma! Entraremos em contato. &#10004;";
+        mensagemContato.style.color = "#fff"; // Define a cor da mensagem
+    }
+});
